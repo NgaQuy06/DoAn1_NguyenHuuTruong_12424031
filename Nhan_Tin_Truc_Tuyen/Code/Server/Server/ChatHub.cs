0a4aae9771882eb8100891417bec3dcefc7380f6
NgaQuy06 <cubietgay@gmail.com>
2026-04-02 19:20:49 +0700

    add connection

--- Nhan_Tin_Truc_Tuyen/Code/Server/Server/ChatHub.cs
+++ Nhan_Tin_Truc_Tuyen/Code/Server/Server/ChatHub.cs
@@ -4,9 +4,19 @@ namespace Server
 {
     public class ChatHub : Hub // Trung tâm kết nối
     {
-        public async Task SendMessage(string user, string message)
+        public async Task SendMessage(string user, string message) // Client phải đặt tên hàm như này để gửi cho server
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message); // Gửi cho tất cả Client, ReceiveMessage: Client phải đặt tên hàm như này để nhận dữ liệu
+        }
+
+        public async Task PingVao(string user)
+        {
+            await Clients.All.SendAsync("ReceivePing", user);
+        }
+
+        public async Task PingRa(string user)
+        {
+            await Clients.All.SendAsync("ReceivePing", user);
         }
     }
 }
