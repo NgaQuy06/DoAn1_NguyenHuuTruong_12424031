32092c4dd5c2698bcdc706c83f895b71c70bc0b3
NgaQuy06 <cubietgay@gmail.com>
2026-04-02 09:38:36 +0700

    Add files via upload

---
+++ Nhan_Tin_Truc_Tuyen/Code/Server/Server/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")] // controller = test
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet] // /api/test
+        public IActionResult Get()
+        {
+            return Ok("Hello từ API 🚀");
+        }
+
+        [HttpPost("login")] // /api/test/login
+        public IActionResult Login([FromBody] LoginRequest req)
+        {
+            if (req.Username == "admin" && req.Password == "1234")
+            {
+                return Ok(new { message = "Đăng nhập thành công" });
+            }
+            return BadRequest(new { message = "Sai tài khoản" });
+        }
+    }
+}
