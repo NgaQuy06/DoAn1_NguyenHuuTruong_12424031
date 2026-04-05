562429d1bcb924541db36b0bb6e0d439136c8737
NgaQuy06 <cubietgay@gmail.com>
2026-04-05 21:54:19 +0700

    Add files via upload

--- Nhan_Tin_Truc_Tuyen/Code/Server/Server/LoginRequest.cs
+++ Nhan_Tin_Truc_Tuyen/Code/Server/Server/LoginRequest.cs
@@ -2,7 +2,9 @@
 {
     public class LoginRequest
     {
-        public string Username { get; set; }
-        public string Password { get; set; }
+        public string Type { get; set; } = ""; 
+        public string Username { get; set; } = "";
+        public string Password { get; set; } = "";
+        public string Email { get; set; } = "";
     }
 }
