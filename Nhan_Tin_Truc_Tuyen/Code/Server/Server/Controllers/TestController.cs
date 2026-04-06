bb26730d0a9d3a16a886e73ae4345a02f4b83960
NgaQuy06 <cubietgay@gmail.com>
2026-04-06 11:50:45 +0700

    Add files via upload

--- Nhan_Tin_Truc_Tuyen/Code/Server/Server/Controllers/TestController.cs
+++ Nhan_Tin_Truc_Tuyen/Code/Server/Server/Controllers/TestController.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DotNetEnv;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.AspNetCore.Mvc;
 using Npgsql;
 using System.Reflection.PortableExecutable;
-using DotNetEnv;
 
 namespace Server.Controllers
 {
@@ -13,7 +14,7 @@ namespace Server.Controllers
         [HttpGet] // /api/test
         public IActionResult Get()
         {
-            return Ok("Hello từ API 🚀");
+            return Ok("Hello mn");
         }
 
         string str = "Host=aws-1-ap-northeast-1.pooler.supabase.com;" +
@@ -32,47 +33,56 @@ namespace Server.Controllers
             {
                 conn = new NpgsqlConnection(str);
                 conn.Open();
-                if (req.Type.Trim() == "dangnhap")
+                string sql = "SELECT * FROM public.\"TaiKhoan\" WHERE \"TenTK\" = @u AND \"MatKhau\" = @p";
+                using (var cmd = new NpgsqlCommand(sql, conn))
                 {
-                    string sql = "SELECT * FROM public.\"TaiKhoan\" WHERE \"TenTK\" = @u AND \"MatKhau\" = @p";
-                    using (var cmd = new NpgsqlCommand(sql, conn))
+                    cmd.Parameters.AddWithValue("u", req.Username.Trim());
+                    cmd.Parameters.AddWithValue("p", req.Password.Trim());
+                    var reader = cmd.ExecuteReader();
+                    if (reader.Read())
                     {
-                        cmd.Parameters.AddWithValue("u", req.Username.Trim());
-                        cmd.Parameters.AddWithValue("p", req.Password.Trim());
-                        var reader = cmd.ExecuteReader();
-                        if (reader.Read())
-                        {
-                            return Ok(new { message = "Đăng nhập thành công" });
-                        }
-                        else
-                        {
-                            return BadRequest(new { message = "Sai tài khoản" });
-                        }
+                        return Ok(new { message = "Đăng nhập thành công" });
+                    }
+                    else
+                    {
+                        return BadRequest(new { message = "Sai tài khoản hoặc mật khẩu" });
                     }
                 }
-                else if (req.Type.Trim() == "dangky")
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { message = e.Message });
+            }
+        }
+
+        [HttpPost("register")] // /api/test/register
+        public IActionResult Register([FromBody] RegisterRequest req)
+        {
+            NpgsqlConnection conn;
+            try
+            {
+                conn = new NpgsqlConnection(str);
+                conn.Open();
+                string sql = "INSERT INTO public.\"TaiKhoan\" (\"TenTK\", \"MatKhau\", \"Email\", \"TrangThai\", \"NgayTao\") VALUES (@a, @b, @c, @d, @e)";
+                using (var cmd = new NpgsqlCommand(sql, conn))
                 {
-                    string sql = "INSERT INTO public.\"TaiKhoan\" (\"TenTK\", \"MatKhau\", \"Email\", \"NgayTao\") VALUES (@a, @b, @c, @d)";
-                    using (var cmd = new NpgsqlCommand(sql, conn))
-                    {
-                        cmd.Parameters.AddWithValue("a", req.Username.Trim());
-                        cmd.Parameters.AddWithValue("b", req.Password.Trim());
+                    cmd.Parameters.AddWithValue("a", req.Username.Trim());
+                    cmd.Parameters.AddWithValue("b", req.Password.Trim());
+                    if (string.IsNullOrWhiteSpace(req.Email))
+                        cmd.Parameters.AddWithValue("c", DBNull.Value);
+                    else
                         cmd.Parameters.AddWithValue("c", req.Email.Trim());
-                        cmd.Parameters.AddWithValue("d", DateTime.Now);
-                        int reader = cmd.ExecuteNonQuery();
-                        if (reader > 0)
-                        {
-                            return Ok(new { message = "Đăng ký thành công" });
-                        }
-                        else
-                        {
-                            return BadRequest(new { message = "Đăng ký thất bại" });
-                        }
+                    cmd.Parameters.AddWithValue("d", "Không hoạt động");
+                    cmd.Parameters.AddWithValue("e", DateTime.Now);
+                    int reader = cmd.ExecuteNonQuery();
+                    if (reader > 0)
+                    {
+                        return Ok(new { message = "Đăng ký thành công" });
                     }
-                }
-                else
-                {
-                    return BadRequest(new { message = "Yêu cầu không hợp lệ" });
+                    else
+                    {
+                        return BadRequest(new { message = "Đăng ký thất bại" });
+                    }
                 }
             }
             catch (Exception e)
