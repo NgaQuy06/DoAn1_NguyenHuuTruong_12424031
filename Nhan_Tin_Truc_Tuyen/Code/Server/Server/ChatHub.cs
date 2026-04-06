a36310d85305b53fd72842fbb5f3abc5d6054d7f
NgaQuy06 <cubietgay@gmail.com>
2026-04-06 12:00:31 +0700

    Add files via upload

--- Nhan_Tin_Truc_Tuyen/Code/Server/Server/ChatHub.cs
+++ Nhan_Tin_Truc_Tuyen/Code/Server/Server/ChatHub.cs
@@ -1,22 +1,26 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using Npgsql;
 
 namespace Server
 {
     public class ChatHub : Hub // Trung tâm kết nối
     {
+        string str = "Host=aws-1-ap-northeast-1.pooler.supabase.com;" +
+                     "Port=6543;" +
+                     "Database=postgres;" +
+                     "Username=postgres.fauxrzhhtdiesxfxuftz;" +
+                     "Password=Nguyentrg2006$;" +
+                     "SSL Mode=Require;" +
+                     "Trust Server Certificate=true;";
+
         public async Task SendMessage(string user, string message) // Client phải đặt tên hàm như này để gửi cho server
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message); // Gửi cho tất cả Client, ReceiveMessage: Client phải đặt tên hàm như này để nhận dữ liệu
         }
-
-        public async Task PingVao(string user)
+        public async Task Logout(string user)
         {
-            await Clients.All.SendAsync("ReceivePing", user);
-        }
-
-        public async Task PingRa(string user)
-        {
-            await Clients.All.SendAsync("ReceivePing", user);
+            
+            await Clients.Caller.SendAsync("Logout", mess);
         }
     }
 }
