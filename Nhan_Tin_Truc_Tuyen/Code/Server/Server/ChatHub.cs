5be3e3f410c6d4b7502bee57cbabb4aecef6d0e1
NgaQuy06 <cubietgay@gmail.com>
2026-04-06 12:04:54 +0700

    Add files via upload

--- Nhan_Tin_Truc_Tuyen/Code/Server/Server/ChatHub.cs
+++ Nhan_Tin_Truc_Tuyen/Code/Server/Server/ChatHub.cs
@@ -5,22 +5,10 @@ namespace Server
 {
     public class ChatHub : Hub // Trung tâm kết nối
     {
-        string str = "Host=aws-1-ap-northeast-1.pooler.supabase.com;" +
-                     "Port=6543;" +
-                     "Database=postgres;" +
-                     "Username=postgres.fauxrzhhtdiesxfxuftz;" +
-                     "Password=Nguyentrg2006$;" +
-                     "SSL Mode=Require;" +
-                     "Trust Server Certificate=true;";
 
         public async Task SendMessage(string user, string message) // Client phải đặt tên hàm như này để gửi cho server
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message); // Gửi cho tất cả Client, ReceiveMessage: Client phải đặt tên hàm như này để nhận dữ liệu
-        }
-        public async Task Logout(string user)
-        {
-            
-            await Clients.Caller.SendAsync("Logout", mess);
         }
     }
 }
--- Nhan_Tin_Truc_Tuyen/Code/Server/Server/RegisterRequest.cs
+++ Nhan_Tin_Truc_Tuyen/Code/Server/Server/RegisterRequest.cs
@@ -1,8 +1,9 @@
 ﻿namespace Server
 {
-    public class LoginRequest
+    public class RegisterRequest
     {
         public string Username { get; set; } = "";
         public string Password { get; set; } = "";
+        public string Email { get; set; } = "";
     }
 }
