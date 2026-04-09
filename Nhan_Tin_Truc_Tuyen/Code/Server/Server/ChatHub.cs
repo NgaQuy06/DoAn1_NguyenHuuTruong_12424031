8410083b346f67104b5a29c3a9676f25d5c9b6a7
NgaQuy06 <cubietgay@gmail.com>
2026-04-09 20:07:52 +0700

    update chathub

--- Nhan_Tin_Truc_Tuyen/Code/Server/Server/ChatHub.cs
+++ Nhan_Tin_Truc_Tuyen/Code/Server/Server/ChatHub.cs
@@ -10,5 +10,15 @@ namespace Server
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message); // Gửi cho tất cả Client, ReceiveMessage: Client phải đặt tên hàm như này để nhận dữ liệu
         }
+
+        public async Task InApp(string user)
+        {
+            await Clients.All.SendAsync("Notification", user + " đã tham gia ứng dụng");
+        }
+
+        public async Task OutApp(string user)
+        {
+            await Clients.All.SendAsync("Notification", user + " đã rời khỏi ứng dụng");
+        }
     }
 }
