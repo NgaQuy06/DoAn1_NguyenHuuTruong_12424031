94612d43874dabc5d0dcd8b64a355d019f87742b
NgaQuy06 <cubietgay@gmail.com>
2026-05-03 10:23:24 +0700

    Change DangKy method to use HttpPost attribute

--- Nhan_Tin_Truc_Tuyen/Code/Server/Server/Controllers/LController.cs
+++ Nhan_Tin_Truc_Tuyen/Code/Server/Server/Controllers/LController.cs
@@ -33,7 +33,7 @@ namespace Server.Controllers
             }
         }
 
-        [HttpGet("DangKy")]
+        [HttpPost("DangKy")]
         public IActionResult DangKy([FromBody] RegisterRequest req)
         {
             try
